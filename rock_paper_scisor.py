ceaad4d164d0fe3a5eeba026c0b97797f7382591
Madalin Vidrighin <vidrighinmadalin@gmail.com>
2025-11-26 20:50:30 +0200

    added validation and win conditions

--- rock_paper_scisor.py
+++ rock_paper_scisor.py
@@ -1,18 +1,39 @@
 import random
 
 choices = ('r' , 'p', 's')
-user_choice = input("rock ,paper or scissor?(r/p/s)").lower()
-if user_choice not in choices:
-    print("Invalid choice")
-
 emoji = {
     'r':'🪨',
     's':'✂️',
     'p':'📃'
 }
 
-pc_choice = random.choice(choices)
+
+while True:
+
+    user_choice = input("rock ,paper or scissor?(r/p/s)").lower()
+    if user_choice not in choices:
+        print("Invalid choice")
+        continue
 
 
-print(f"you chose {emoji[user_choice]}")
-print(f"pc chose {emoji[pc_choice]}")
+    pc_choice = random.choice(choices)
+
+    print(f"you chose {emoji[user_choice]}")
+    print(f"pc chose {emoji[pc_choice]}")
+
+    if user_choice == pc_choice:
+        print("Tie")
+    elif \
+        (user_choice == 'r' and pc_choice == 's') or \
+        (user_choice == 's' and pc_choice=='p') or\
+        (user_choice == 'p' and pc_choice == 'r'):
+        print("You win")
+    else:
+        print("You lose")
+        
+        
+    should_continue = input("Comtinue(y/n): ")
+
+    if should_continue == 'n':
+        break
+    
